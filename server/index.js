903fb61f47c9c6ceadb835b6b307bce0c879b534
IHCantabraba <inigocanta@gmail.com>
2024-12-22 16:32:21 +0100

    DB connection

---
+++ server/index.js
@@ -0,0 +1,32 @@
+import express from 'express'
+import dotenv from 'dotenv'
+dotenv.config()
+import connectDB from './src/config/db.js'
+import cors from 'cors'
+// import userRouter from './src/api/routes/users.js'
+import userRouter from './src/api/routes/users2.js'
+import authRouter from './src/api/routes/auth.js'
+import cloudinaryConfig from './src/config/cloudinary.js'
+
+const app = express()
+const PORT = 3000
+
+connectDB()
+cloudinaryConfig()
+/* configure to accept json structure */
+app.use(express.json())
+/* configure to access from different ip (front and back end) */
+app.use(cors())
+/* router a los distintos modelos */
+/* auth (register login) */
+app.use('/api/auth', authRouter)
+/* users */
+app.use('/api/users', userRouter)
+/* Not found Root */
+app.use('*', (req, res, next) => {
+  return res.status(404).json(`Route not found`)
+})
+/* listen port for backend */
+app.listen(PORT, () => {
+  console.log(`Server running at: http://localhost:${PORT}`)
+})
--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,8 +1,10 @@
 import mongoose from 'mongoose'
 
 const connectDB = async () => {
+  console.log(`uri is: ${process.env.DB_URL}`)
   try {
     await mongoose.connect(process.env.DB_URL)
+
     console.log(`Successfully connected to DB 😃`)
   } catch (error) {
     console.log(`Error occurred while connecting to DB: ${error}`)
