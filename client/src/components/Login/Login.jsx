98d61352786f79bcd7139075f10ea23cde1c652f
IHCantabraba <inigocanta@gmail.com>
2024-12-22 17:27:52 +0100

    request send

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -13,7 +13,7 @@ import { Close, Send } from '@mui/icons-material'
 import PasswordField from '../Password/PasswordField'
 import { useEffect, useRef, useState } from 'react'
 import GoogleOneTapLogin from '../GoogleLogin/GoogleLogin'
-
+const BaseUrl = import.meta.env.VITE_BaseName
 const Login = () => {
   /* obetner estado inicial de nuestro custom hook y el dispatcher para cambair estados */
   /* el Login se va a habrir desde varios sitios por lo que se necesita controlar el estadp con una variable"openLogin" y el dispatch para cerrarlo  */
@@ -36,27 +36,67 @@ const Login = () => {
   const handleClose = () => {
     dispatch({ type: 'CLOSE_LOGIN' })
   }
-  /* controlar el formulario */
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    //testing loading
-    dispatch({ type: 'START_LOADING' })
-    setTimeout(() => {
-      dispatch({ type: 'END_LOADING' })
-    }, 6000)
-    /* testing NOTIS */
-    const password = passwordRef.current.value
-    const confirmPassword = confirmPasswordRef.current.value
-    if (password !== confirmPassword) {
+
+  /* testing login and register requests */
+  const handleLogin = async () => {
+    const userName = nameRef.current.value
+    const pass = passwordRef.current.value
+    console.log(userName, pass)
+    // if (!isRegister) {
+    //   const user = nameRef.current.value
+    //   const password = passwordRef.current.value
+    //   console.log(user, password)
+    // }
+    try {
+      const fetchResults = await fetch(`${BaseUrl}/api/auth/login`, {
+        headers: new Headers({
+          'Content-Type': 'Application/json'
+        }),
+        method: 'POST',
+        body: JSON.stringify({ nombre: userName, password: pass })
+      })
+      if (fetchResults.ok) {
+        dispatch({
+          type: 'UPDATE_ALERT',
+          payload: {
+            open: true,
+            message: 'login succcesfully'
+          }
+        })
+      }
+    } catch (error) {
       dispatch({
         type: 'UPDATE_ALERT',
         payload: {
           open: true,
           severity: 'error',
-          message: 'Passwords do not match'
+          message: `Bad request ${error}`
         }
       })
     }
+  }
+  /* controlar el formulario */
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    handleLogin()
+    //testing loading
+    // dispatch({ type: 'START_LOADING' })
+    // setTimeout(() => {
+    //   dispatch({ type: 'END_LOADING' })
+    // }, 6000)
+    // /* testing NOTIS */
+    // const password = passwordRef.current.value
+    // const confirmPassword = confirmPasswordRef.current.value
+    // if (password !== confirmPassword) {
+    //   dispatch({
+    //     type: 'UPDATE_ALERT',
+    //     payload: {
+    //       open: true,
+    //       severity: 'error',
+    //       message: 'Passwords do not match'
+    //     }
+    //   })
+    // }
   }
   /* ustilizar el hook useEfect para cambiar el título del dialogo entre Login/Register */
   useEffect(() => {
@@ -83,33 +123,33 @@ const Login = () => {
           <DialogContentText>
             Please fill you information in the fields below:
           </DialogContentText>
+          <TextField
+            autoFocus
+            margin='normal'
+            variant='standard'
+            id='name'
+            label='Name'
+            type='text'
+            fullWidth
+            inputRef={nameRef}
+            inputProps={{ minLength: 2 }}
+            required
+          />
           {isRegister && (
             <TextField
-              autoFocus
+              /* si no esta registrado el focus lo hacemos al mail */
+              autoFocus={!isRegister}
               margin='normal'
               variant='standard'
-              id='name'
-              label='Name'
-              type='text'
-              ref={nameRef}
+              id='email'
+              label='Email'
+              type='email'
               fullWidth
-              inputRef={nameRef}
-              inputProps={{ minLength: 2 }}
+              inputRef={emailRef}
               required
             />
           )}
-          <TextField
-            /* si no esta registrado el focus lo hacemos al mail */
-            autoFocus={!isRegister}
-            margin='normal'
-            variant='standard'
-            id='email'
-            label='Email'
-            type='email'
-            fullWidth
-            inputRef={emailRef}
-            required
-          />
+
           {/* añadir campo de password y si estamos en el menu register, añadir el campo password de valdiacion Al primero, solo se le pasa la prop del useRef daod que el resto usa los campos por defecto */}
           <PasswordField {...{ passwordRef }} />
           {isRegister && (
